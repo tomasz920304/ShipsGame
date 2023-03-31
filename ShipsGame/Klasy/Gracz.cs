cdc660cd6a109e1a134d46f911f2f7bf94a37230
Tomasz Grzywacz <tomaszgrzywacz92@gmail.com>
2023-03-31 19:38:18 +0200

    ShipsGame

--- ShipsGame/Klasy/Gracz.cs
+++ ShipsGame/Klasy/Gracz.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ShipsGame.Klasy
 {
-    internal class Gracz
+    public class Gracz
     {
         public string Nazwa;
         // zestaw wszystkich pól gracza
